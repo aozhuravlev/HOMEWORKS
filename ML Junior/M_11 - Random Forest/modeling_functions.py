db314432e3afa823794f519ada690c27a9fa2e0b
Aleksey Zhuravlev <a.o.zhuravlev@gmail.com>
2024-11-20 16:24:53 +0300

    logs applied to target

--- ML Junior/M_11 - Random Forest/modeling_functions.py
+++ ML Junior/M_11 - Random Forest/modeling_functions.py
@@ -10,7 +10,7 @@ from sklearn.metrics import r2_score
 
 
 def tune_model(model, params, X_train, y_train):
-    search = GridSearchCV(model, params, scoring="r2", cv=3, n_jobs=-1, verbose=2)
+    search = GridSearchCV(model, params, scoring="r2", cv=5, n_jobs=-1, verbose=2)
     search.fit(X_train, y_train)
     print(
         f"{'The best parameters for':20} {model.__class__.__name__}:\n{search.best_params_}\n"
@@ -23,6 +23,7 @@ def get_r2_score(models, splits):
     for model in models:
         model.fit(X_train, y_train) 
         y_pred = model.predict(X_test)
+        # y_pred = np.expm1(model.predict(X_test))
         r2 = r2_score(y_test, y_pred)
         print(f"R2 score for {type(model).__name__}: {r2:.2%}")    
 
@@ -31,6 +32,7 @@ def get_mae_score(models, splits):
     for model in models:
         model.fit(X_train, y_train) 
         y_pred = model.predict(X_test)
+        # y_pred = np.expm1(model.predict(X_test))
         mae = mean_absolute_error(y_test, y_pred)
         print(f"MAE score for {type(model).__name__}: {mae:.2f}")
 
