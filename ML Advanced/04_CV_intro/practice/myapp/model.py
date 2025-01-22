5f6cb8989a3db89948635fab9797b25477781b03
aozhuravlev <a.o.zhuravlev@gmail.com>
2025-01-22 18:25:51 +0300

    dilate & norm added

--- ML Advanced/04_CV_intro/practice/myapp/model.py
+++ ML Advanced/04_CV_intro/practice/myapp/model.py
@@ -4,26 +4,53 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+from scipy.ndimage import binary_dilation
+
 
 # Определение класса SimpleCNN (если он не импортируется из другого модуля)import torch
 class SimpleCNN(nn.Module):
     def __init__(self, num_classes):
         super(SimpleCNN, self).__init__()
+
+        # Свёрточные слои + BatchNorm
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.fc1 = nn.Linear(64 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+
+        # Полносвязные слои + BatchNorm + Dropout
+        self.fc1 = nn.Linear(128 * 3 * 3, 1024)
+        self.bn_fc1 = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.bn_fc2 = nn.BatchNorm1d(512)
+        self.fc3 = nn.Linear(512, num_classes)
+
+        # self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
-        x = x.view(-1, 1, 28, 28)  # Исправление: изменение размерности входных данных
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)  # Автоматическое разворачивание
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        x = x.view(
+            -1, 1, 28, 28
+        )  # Убедимся, что входные данные имеют нужную размерность
 
+        x = self.pool(F.leaky_relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.leaky_relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.leaky_relu(self.bn3(self.conv3(x))))
+
+        x = x.view(x.size(0), -1)  # Разворачивание перед FC-слоями
+
+        # x = self.dropout(F.relu(self.bn_fc1(self.fc1(x))))
+        # x = self.dropout(F.relu(self.bn_fc2(self.fc2(x))))
+        x = F.leaky_relu(self.bn_fc1(self.fc1(x)))
+        x = F.leaky_relu(self.bn_fc2(self.fc2(x)))
+        x = self.fc3(x)
+
+        return x
 
 
 # Класс для загрузки модели и выполнения предсказаний
@@ -55,6 +82,14 @@ class Model:
         pred : str
             Символ-предсказание
         """
+        # Применяем дилатацию
+        x = binary_dilation(x).astype(np.float32)
+
+        # Нормализация
+        mean = 0.1307
+        std = 0.3081
+        x = (x - mean) / std
+
         # Преобразуем входное изображение в тензор
         x = torch.from_numpy(x).float()
         x = x.unsqueeze(0).unsqueeze(0)
